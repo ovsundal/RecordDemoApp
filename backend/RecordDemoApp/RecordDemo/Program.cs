28b8149d82d0a10d32aa4a08d649ddf6c47cfe27
osund <osundal83@gmail.com>
2022-06-04 01:08:03 +0200

    add when to use/not use records description

--- backend/RecordDemoApp/RecordDemo/Program.cs
+++ backend/RecordDemoApp/RecordDemo/Program.cs
@@ -1,5 +1,22 @@
 ﻿using System.Threading.Channels;
 
+/**
+ * Benefits of Records:
+ * - Simple to set up
+ * - Thread-safe
+ * Easy / safe to share
+ *
+ * When to use Records:
+ * - Capturing external data that doesn't change - WeatherService, SWAPI.dev
+ * - API calls
+ * - Processing data
+ * - Read-only data
+ *
+ * When not to use Records:
+ * - When you need to change the data (Entity Framework)
+ * 
+ */
+
 Record1 r1a = new("Tim", "Corey");
 Record1 r1b = new("Tim", "Corey");
 Record1 r1c = new("Sue", "Storm");
@@ -45,9 +62,39 @@ Record1 r1d = r1a with
 
 Console.WriteLine($"Jon's record: {r1d}");
 
+Console.WriteLine();
+Record2 r2a = new("Tim", "Corey");
+Console.WriteLine($"R2a Value: {r2a}");
+Console.WriteLine($"R2a fn: {r2a.FirstName}  ln: {r2a.LastName}");
+Console.WriteLine(r2a.SayHello());
+
 // a Record is just a fancy class
 // Immutable - The values cannot be changed
 public record Record1(string FirstName, string LastName);
+
+// inheritance
+public record User1(int Id, string FirstName, string LastName) : Record1(FirstName, LastName);
+
+public record Record2(string FirstName, string LastName)
+{
+    private string _firstName = FirstName;
+
+    public string FirstName
+    {
+        get { return _firstName.Substring(0, 1); }
+        init { _firstName = value; }
+    }
+    
+    public string FullName
+    {
+        get => $"{FirstName} {LastName}";
+    }
+
+    public string SayHello()
+    {
+        return $"Hello {FirstName}";
+    }
+}
 
 public class Class1
 {
@@ -65,6 +112,15 @@ public class Class1
         FirstName = this.FirstName;
         LastName = this.LastName;
     }
+    
+    //***************************
+    // DO NOT DO ANY OF THE BELOW
+    //***************************
+    public record Record3 // No constructor so no deconstructor
+    {
+        public string FirstName { get; set; } // The set makes this record mutable (BAD!)
+        public string LastName { get; set; } // The set makes this record mutable (BAD!)
+    }
+    
+    // Don't just make clones all over to update state
 }
-// https://www.youtube.com/watch?v=9Byvwa9yF-I
-// 36:17
